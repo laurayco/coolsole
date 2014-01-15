7296e2209bb5a6cd152ef7f449df0c2706e2f3a4
Full Metal <tyler9725@gmail.com>
2014-01-15 11:20:53 -0600

    more diversified test code.
    lulz.

--- src/test-main.cpp
+++ src/test-main.cpp
@@ -3,16 +3,44 @@
 
 using namespace Coolsole;
 
+using namespace Coolsole;
+
 int main(int argc,char** argv)
 {
     FormattedOutput& format_control = ConsoleOutput::Singleton.get_instance();
-
-    format_control.background(Blue);
-    std::cout<<"\tWelcome..."<<std::endl;
-    format_control.previous_background();
-    std::cout<<"\t";
-    format_control.foreground(BrightWhite);
-    format_control.background(Red);
-    std::cout<<"\t\tTo the future.\t\t"<<std::endl;
-
+    
+    Color color_cycle[] = {
+        Black,
+        Blue,
+        Red,
+        White,
+        Green,
+        Purple,
+        Aqua,
+        Gray,
+        BrightBlue,
+        BrightRed,
+        BrightWhite,
+        BrightGreen,
+        BrightPurple,
+        BrightAqua
+    };
+    
+    for(int i=0;i<(sizeof(color_cycle)/sizeof(Color));i++)
+    {
+        format_control.foreground(color_cycle[i]);
+        std::cout<<"Testing:"<<color_cycle[i]<<"::"<<i<<std::endl;
+        format_control.previous_foreground();
+    }
+    
+    format_control.reset();
+    
+    for(int i=0;i<(sizeof(color_cycle)/sizeof(Color));i++)
+    {
+        format_control.background(color_cycle[i]);
+        std::cout<<"Testing:"<<color_cycle[i]<<"::"<<i<<std::endl;
+        format_control.previous_background();
+    }
+    
+    return 0;
 }
