0f86d6994fe950a3c9ed60bb9acc5d42decf7b76
Full Metal <tyler9725@gmail.com>
2014-01-14 21:32:00 -0600

    windows basically working
    I need to work out some access keywords but other than that this thing
    is ALL.GOOD.

---
+++ include/coolsole.hpp
@@ -0,0 +1,162 @@
+#ifndef __coolsole__hpp
+
+#include <iostream>
+#include <stack>
+
+#ifdef _WIN32
+	#include <windows.h>
+#endif
+
+#ifndef nullptr
+#define nullptr NULL
+#endif
+
+namespace Coolsole
+{
+
+	class ColorState;
+	class FormattedOutput;
+
+	template<typename>
+	class SingletonContainer;
+
+	/*
+		Color Codes found here:
+			http://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+	*/
+	enum Color {
+			Black = 0,
+			Blue = 1,
+			Green = 2,
+			Aqua = 3,
+			Red = 4,
+			Purple = 5,
+			Yellow = 6,
+			White = 7,
+			Gray = 8,
+			BrightBlue = 9,
+			BrightGreen = 10,
+			BrightAqua = 11,
+			BrightRed = 12,
+			BrightPurple = 13,
+			BrightYellow = 14,
+			BrightWhite = 15
+	};
+
+
+	/*
+		This class is entirely not
+		necessary. It's just here
+		for the sake of inheritance,
+		and practical extendability.
+	*/
+	class ColorState
+	{
+	protected:
+		std::stack<Color> colors;
+	public:
+		virtual void push_color(Color);
+		virtual Color pop_color();
+		operator Color () const;
+		Color current_color() const;
+		ColorState(Color);
+	};
+
+	/*
+		Again, here simply for the purpose
+		of inheritance. Maybe adding an
+		HTML-output could be a good idea.
+		Could base this on detecting
+		CGI, and returnning a HTML-output
+		instance in the get_console()
+		function or something. That would
+		be pretty cool, jyah?
+	*/
+	class FormattedOutput
+	{
+	public:
+		Color foreground() const;
+		Color background() const;
+		void foreground(Color);
+		void background(Color);
+		void previous_foreground();
+		void previous_background();
+	protected:
+		ColorState _foreground, _background;
+		FormattedOutput();
+		virtual void set_state(Color,Color)=0;
+	};
+
+	template<typename Type>
+	class SingletonContainer
+	{
+		protected:
+			Type* instance;
+			virtual Type* make_instance() = 0;
+		public:
+		SingletonContainer(Type* ptr = nullptr):
+			instance(ptr)
+		{
+		}
+
+		Type& get_instance()
+		{
+			if(instance!=nullptr)
+			{
+				return *instance;
+			}
+			instance = make_instance();
+			return *instance;
+		}
+
+		~SingletonContainer()
+		{
+			if(instance!=nullptr)
+			{
+				delete instance;
+			}
+		}
+	};
+
+	/*
+		Because only std::out will support
+		any kind of text formatting,
+		we will make this a singleton type
+		object. Notice however, that this
+		singleton instance does inherit
+		from the potentially non-singleton
+		FormattedOutput class.
+	*/
+	class ConsoleOutput: public FormattedOutput
+	{
+	protected:
+		class ConsoleSingleton: public SingletonContainer<ConsoleOutput>
+		{
+			protected:
+				virtual ConsoleOutput* make_instance()
+				{
+					#ifdef _WIN32
+						return new ConsoleOutput(GetStdHandle( STD_OUTPUT_HANDLE ));
+					#else
+						return new ConsoleOutput();
+					#endif
+				}
+		};
+		virtual void set_state(Color,Color);
+		#ifdef _WIN32
+			ConsoleOutput(HANDLE);
+		#else
+			ConsoleOutput();
+		#endif
+		virtual ~ConsoleOutput();
+	public:
+		#ifdef _WIN32
+		    HANDLE console_handle;
+			CONSOLE_SCREEN_BUFFER_INFO original_csbi;
+		#endif
+		static ConsoleSingleton Singleton;
+	};
+
+}
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,144 @@
+#include <iostream>
+#include <windows.h>
+#include "../include/coolsole.hpp"
+
+Coolsole::ColorState::ColorState(Coolsole::Color initial)
+{
+	push_color(initial);
+}
+
+void Coolsole::ColorState::push_color(Coolsole::Color color)
+{
+	colors.push(color);
+}
+
+Coolsole::Color Coolsole::ColorState::pop_color()
+{
+	Coolsole::Color r = static_cast<Coolsole::Color>(*this);
+	colors.pop();
+	return r;
+}
+
+Coolsole::Color Coolsole::ColorState::current_color() const
+{
+	return this->colors.top();
+}
+
+
+Coolsole::ColorState::operator Coolsole::Color() const
+{
+	return this->current_color();
+}
+
+Coolsole::Color Coolsole::FormattedOutput::foreground() const
+{
+	return this->_foreground.current_color();
+}
+
+Coolsole::Color Coolsole::FormattedOutput::background() const
+{
+	return this->_background.current_color();
+}
+
+void Coolsole::FormattedOutput::foreground(Coolsole::Color c)
+{
+	this->_foreground.push_color(c);
+	this->_background.push_color(this->_background);
+	this->set_state(this->foreground(),this->background());
+}
+
+void Coolsole::FormattedOutput::background(Coolsole::Color c)
+{
+	this->_background.push_color(c);
+	this->_foreground.push_color(this->_foreground);
+	this->set_state(this->foreground(),this->background());
+}
+
+Coolsole::FormattedOutput::FormattedOutput():
+	_foreground(Coolsole::White),
+	_background(Coolsole::Black)
+{
+}
+
+void Coolsole::FormattedOutput::previous_foreground()
+{
+	// notice: colors are kept consistent.
+	//      ie: if foreground is set, the
+	//          the background is replicated.
+	this->_foreground.pop_color();
+	this->_foreground.push_color(this->foreground());
+	this->set_state(this->foreground(),this->background());
+}
+
+void Coolsole::FormattedOutput::previous_background()
+{
+	// notice: colors are kept consistent.
+	//      ie: if foreground is set, the
+	//          the background is replicated.
+	this->_background.pop_color();
+	this->_background.push_color(this->background());
+	this->set_state(this->foreground(),this->background());
+}
+
+#ifdef _WIN32
+	#include <windows.h>
+
+	Coolsole::ConsoleOutput::ConsoleSingleton Coolsole::ConsoleOutput::Singleton();
+
+	Coolsole::ConsoleOutput::ConsoleOutput(HANDLE h):
+		Coolsole::FormattedOutput()
+	{
+		this->console_handle = h;
+		GetConsoleScreenBufferInfo( this->console_handle, &(this->original_csbi) );
+		std::cout<<"Original: "<<this->original_csbi.wAttributes<<std::endl;
+	}
+
+	void Coolsole::ConsoleOutput::set_state(Coolsole::Color fg,Coolsole::Color bg)
+	{
+		SetConsoleTextAttribute(
+			Coolsole::ConsoleOutput::console_handle,
+			(bg<<4)|fg
+		);
+	}
+
+	Coolsole::ConsoleOutput::~ConsoleOutput()
+	{
+		SetConsoleTextAttribute(
+			this->console_handle,
+			this->original_csbi.wAttributes
+		);
+	}
+#else
+
+	Coolsole::ConsoleOutput& Coolsole::ConsoleOutput::get_console()
+	{
+		if(Coolsole::ConsoleOutput::output!=nullptr){
+			return *Coolsole::ConsoleOutput::output;
+		}
+		Coolsole::ConsoleOutput::output = new Coolsole::ConsoleOutput();
+		return *Coolsole::ConsoleOutput::output;
+	}
+
+	Coolsole::ConsoleOutput::ConsoleOutput(): Coolsole::FormattedOutput()
+	{
+		/*
+			I don't think anything really needs to
+			be done in the constructor. Just here
+			because... ?
+		*/
+	}
+#endif
+
+int main()
+{
+	Coolsole::FormattedOutput& FormatControl = Coolsole::ConsoleOutput::Singleton.get_instance();
+
+	std::cout<<"Testing formatting..."<<std::endl;
+
+	FormatControl.foreground(Coolsole::Red);
+	FormatControl.background(Coolsole::BrightWhite);
+
+	std::cout<<"Did it work!?"<<std::endl;
+
+	return 0;
+}
