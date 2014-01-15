d4d8cf8d67dbc9900067a465c6d3ab184a61c4b9
Full Metal <tyler9725@gmail.com>
2014-01-14 22:25:58 -0600

    created module files.
    filled them with a test program, for the time being. hm.

---
+++ include/coolsolemodule.hpp
@@ -0,0 +1,1 @@
+#include "../include/coolsole.hpp"
---
+++ src/coolsolemodule.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "../include/coolsolemodule.hpp"
+
+using namespace Coolsole;
+
+int main(int argc,char** argv)
+{
+	FormattedOutput& format_control = ConsoleOutput::Singleton.get_instance();
+
+	format_control.background(Blue);
+	std::cout<<"\tWelcome..."<<std::endl;
+	format_control.previous_background();
+	std::cout<<"\t";
+	format_control.foreground(BrightWhite);
+	format_control.background(Red);
+	std::cout<<"\t\tTo the future.\t\t"<<std::endl;
+
+}
