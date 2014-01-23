16d09e9a8b386d985fc5ec7bae03bdf99d1186e0
Full Metal <tyler9725@gmail.com>
2014-01-22 21:31:19 -0600

    defining the base FormattedOutput class
    Could be used for inheriting probably haha amirite.

--- include/coolsolemodule.hpp
+++ include/coolsolemodule.hpp
@@ -5,22 +5,104 @@
 
 #include <Python.h>
 
+typedef struct {
+  PyObject_HEAD
+  /* Type-specific fields go here. */
+  Coolsole::FormattedOutput *__cpp_object;
+
+} coolsole_FormattedOutput;
+
 extern "C" {
   static PyObject *say_hello(PyObject *, PyObject *);
+
+  /* Formatted Output base-class. */
+  static void deallocate_formatted_output(coolsole_FormattedOutput *);
+  static void allocate_formatted_output(PyTypeObject *, PyObject *, PyObject *);
+  static void init_formatted_output(coolsole_FormattedOutput *, PyObject *, PyObject *);
+  /*static PyObject* formatted_output_reset(PyObject*);
+  static PyObject* formatted_output_get_format(PyObject*);
+  static PyObject* formatted_output_set_format(PyObject*,PyObject*);*/
+
   PyMODINIT_FUNC PyInit_hello(void);
 }
 
-static PyMethodDef HelloMethods[2] = {
+static PyMemberDef coolsole_formatted_output_members[] = {
+  {NULL}  /* Has no python-accessible members. Only methods. ( ! ) */
+};
+
+static PyMethodDef coolsole_formatted_output_methods[] = {
+  /*{
+    "reset",
+    (PyCFunction)formatted_output_reset,
+    METH_NOARGS,
+    "Set formatting back to original formatting."
+  }, {
+    "get_format",
+    (PyCFunction)formatted_output_get_format,
+    METH_NOARGS,
+    "Get the current format object. ( READ-ONLY )."
+  }, {
+    "set_format",
+    (PyCFunction)formatted_output_set_format,
+    METH_NOARGS,
+    "Set the current format object. ( READ-ONLY )."
+  },*/
+  {NULL}  /* Sentinel */
+};
+
+static PyTypeObject coolsole_FormattedOutputType = {
+  PyVarObject_HEAD_INIT(NULL, 0)
+  "coolsole.FormattedOutput",/* tp_name */
+  sizeof(coolsole_FormattedOutput),             /* tp_basicsize */
+  0,                         /* tp_itemsize */
+  (destructor)deallocate_formatted_output, /* tp_dealloc */
+  0,                         /* tp_print */
+  0,                         /* tp_getattr */
+  0,                         /* tp_setattr */
+  0,                         /* tp_reserved */
+  0,                         /* tp_repr */
+  0,                         /* tp_as_number */
+  0,                         /* tp_as_sequence */
+  0,                         /* tp_as_mapping */
+  0,                         /* tp_hash  */
+  0,                         /* tp_call */
+  0,                         /* tp_str */
+  0,                         /* tp_getattro */
+  0,                         /* tp_setattro */
+  0,                         /* tp_as_buffer */
+  Py_TPFLAGS_DEFAULT |
+  Py_TPFLAGS_BASETYPE,   /* tp_flags */
+  "Base class for FormattedOutput control.",           /* tp_doc */
+  0,                         /* tp_traverse */
+  0,                         /* tp_clear */
+  0,                         /* tp_richcompare */
+  0,                         /* tp_weaklistoffset */
+  0,                         /* tp_iter */
+  0,                         /* tp_iternext */
+  coolsole_formatted_output_methods,             /* tp_methods */
+  coolsole_formatted_output_members,             /* tp_members */
+  0,                         /* tp_getset */
+  0,                         /* tp_base */
+  0,                         /* tp_dict */
+  0,                         /* tp_descr_get */
+  0,                         /* tp_descr_set */
+  0,                         /* tp_dictoffset */
+  (initproc)init_formatted_output, /* tp_init */
+  0,                         /* tp_alloc */
+  allocate_formatted_output, /* tp_new */
+};
+
+static PyMethodDef CoolsoleMethods[2] = {
   {"say_hello", say_hello, METH_VARARGS, "Greet somebody."},
   {NULL, NULL, 0, NULL}
 };
 
-static struct PyModuleDef hello_module = {
+static struct PyModuleDef coolsole_module = {
   PyModuleDef_HEAD_INIT,
   "coolsole",   /* name of module */
   NULL,      /* module documentation, may be NULL */
   -1,       /* size of per-interpreter state of the module,
                 or -1 if the module keeps state in global variables. */
-  HelloMethods
+  CoolsoleMethods
 };
 #endif
--- src/coolsolemodule.cpp
+++ src/coolsolemodule.cpp
@@ -12,12 +12,50 @@ static PyObject *say_hello(PyObject *self, PyObject *args)
   Py_RETURN_NONE;
 }
 
+/*
+  static void deallocate_formatted_output(coolsole_FormattedOutput*);
+  static void allocate_formatted_output(PyTypeObject*, PyObject*, PyObject*);
+  static void init_formatted_output(coolsole_FormattedOutput*, PyObject *, PyObject *);
+  static PyObject* formatted_output_reset(PyObject*);
+  static PyObject* formatted_output_get_format(PyObject*);
+  static PyObject* formatted_output_set_format(PyObject*,PyObject*);
+*/
+
+static void deallocate_formatted_output(coolsole_FormattedOutput *self)
+{
+  delete self->__cpp_object;
+}
+
+static int init_formatted_output(coolsole_FormattedOutput *self, PyObject *args, PyObject *kwds)
+{
+  /*
+    Since I'm not exposing anything
+    to the external python members,
+    I shouldn't need to do any kind of
+    reference-counting, or assignments
+    here
+  */
+  return 0;
+}
+
+static PyObject *allocate_formatted_output(PyTypeObject *type, PyObject *args, PyObject *kwds)
+{
+  coolsole_FormattedOutput *self = (coolsole_FormattedOutput *)type->tp_alloc(type, 0);
+  self->__cpp_object = new Coolsole::FormattedOutput();
+  return (PyObject *)self;
+}
+
 PyMODINIT_FUNC PyInit_coolsole(void)
 {
   PyObject *m;
-  m = PyModule_Create(&hello_module);
+  if (PyType_Ready(&NoddyType) < 0) {
+    return NULL;
+  }
+  m = PyModule_Create(&coolsole_module);
   if (m == NULL) {
     return NULL;
   }
+  Py_INCREF(&coolsole_FormattedOutputType);
+  PyModule_AddObject(m, "Noddy", (PyObject *)&coolsole_FormattedOutputType);
   return m;
 }
